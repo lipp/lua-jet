156028002501c777134afea1add1920984e53683
Gerhard Lipp <gelipp@gmail.com>
2013-01-18 16:57:00 +0100

    add more echo tests and close event test

--- spec/socket_spec.lua
+++ spec/socket_spec.lua
@@ -7,75 +7,151 @@ local jetsocket = require'jet.socket'
 local loop = ev.Loop.default
 local port = 12349
 
-local echo_listener = assert(socket.bind('*',port))
-local accept_echo = function()
-   local sock = echo_listener:accept()
-   local io = jetsocket.wrap(
-      sock,{
-         loop = loop,
-         on_message = function(wrapped,message)
-            wrapped:send(message)
-         end,
-         on_close = function() end,
-         on_error = function() end
-           }):read_io()
-   io:start(loop)
-end
-echo_listener:settimeout(0)
-local echo_server_io = ev.IO.new(
-   accept_echo,
-   echo_listener:getfd(),
-   ev.READ)
-
 describe(
-   'A message socket', 
+   'Echo test with message socket', 
    function()
+      local echo_server_io      
+      local echo_listener  
       before(
          function()
+            echo_listener = socket.bind('*',port)
+            local accept_echo = function()
+               local sock = echo_listener:accept()
+               local current = jetsocket.wrap(sock)
+               current:on_message(
+                  function(wrapped,message)
+                     wrapped:send(message)
+                  end)
+               current:read_io():start(loop)
+            end
+            echo_listener:settimeout(0)
+            echo_server_io = ev.IO.new(
+               accept_echo,
+               echo_listener:getfd(),
+               ev.READ)            
             echo_server_io:start(loop)
          end)
 
       after(
          function()
             echo_server_io:stop(loop)
+            echo_listener:close()
          end)
 
-      describe(
-         'when connecting to echo server',
+      local sock
+      local echo = function(message,done)
+         local f = function(done)
+            local wrapped = jetsocket.wrap(sock)
+            wrapped:on_message(
+               function(wrapped,echoed)
+                  assert.is.same(message,echoed)
+                  done()                                                
+               end)
+            wrapped:read_io():start(loop)
+            wrapped:send(message)                     
+         end
+         return f
+      end
+      
+      before_each(
+         function()                  
+            sock = socket.connect('localhost',port)
+         end)
+      
+      after_each(
          function()
-            local sock
-            before(
-               function()
-                  sock = socket.connect('localhost',port)
+            sock:close()
+         end)
+      
+      it('can echo numbers',async,echo(1234))
+      it('can echo strings',async,echo('hello'))
+      it('can echo boolean',async,echo(true))
+      it('can echo tables',async,echo{a = 1,b = 3, c = 'aps'})
+      it('can echo nested tables',async,echo{a = { sub = false }})
+      it('can echo arrays',async,echo{1,2,3,4})
+      it('can echo many messages fast',async,
+         function(done)
+            local wrapped = jetsocket.wrap(sock)
+            local count = 0
+            local messages = {
+               '123',{a='HAHA'},false
+            }
+
+            wrapped:on_message(
+               function(wrapped,echoed)
+                  count = count + 1
+                  assert.is.same(messages[count],echoed)
+                  if count == 3 then
+                     done()
+                  end
                end)
 
-            after(
-               function()
-                  sock:close()
-               end)
+            wrapped:read_io():start(loop)
 
-            it(
-               'can echo messages',
-               async,
-               function(done)
-                  local message = {
-                     1,2,3
-                  }
-                  local wrapped = jetsocket.wrap(
-                     sock,{
-                        on_message = function(wrapped,echoed)
-                           assert.is.same(message,echoed)
-                           done()                                                
-                        end,
-                        loop = loop,
-                        on_close = function() end,
-                        on_error = function() end
-                          })
+            for _,message in ipairs(messages) do
+               wrapped:send(message)
+            end
+         end)            
+   end)
+
+describe(
+   'Event test with message socket', 
+   function()
+      local server_io
+      local listener
+      local on_accept
+      before(
+         function()
+            listener,err = socket.bind('*',port)
+            local accept = function()
+               local sock = listener:accept()
+               sock:close()
+            end
+            listener:settimeout(0)
+            server_io = ev.IO.new(
+               accept,
+               listener:getfd(),
+               ev.READ)
+            server_io:start(loop)
+         end)
+      after(
+         function()            
+            server_io:stop(loop)
+            listener:close()
+         end)
+
+            
+      it('should fire on_close event',
+         async,
+         function(done)
+            local sock = socket.tcp()
+            sock:settimeout(0)
+            ev.IO.new(
+               function(loop,connect_io)
+                  connect_io:stop(loop)
+                  local wrapped = jetsocket.wrap(sock)
+                  local timer
+                  wrapped:on_close(
+                     function()
+                        timer:stop(loop)
+                        assert.is_true(true)
+                        done()
+                     end)
+                  timer = ev.Timer.new(
+                     function()
+                        assert(false)
+                        done()
+                        wrapped:read_io():stop(loop)
+                     end,1)
+                  timer:start(loop)
                   wrapped:read_io():start(loop)
-                  wrapped:send(message)                     
-               end)           
+               end,sock:getfd(),ev.WRITE):start(loop) -- connect io
+            sock = socket.connect('localhost',port)
          end)
    end)
 
-return 'ev',loop
+
+return function()
+   loop:loop()
+       end
 
