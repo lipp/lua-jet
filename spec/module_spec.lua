9db67857b842e7e3f070e5df7b458db5479cfae9
Gerhard Lipp <gelipp@gmail.com>
2013-01-30 14:37:16 +0100

    add (global) jet module

---
+++ spec/module_spec.lua
@@ -0,0 +1,38 @@
+require'busted'
+package.path = package.path..'../src'
+
+describe(
+   'The jet (global) module', 
+   function()
+      local jet
+      it(
+	 'can be required',
+	 function()
+	    assert.has.no.errors(
+	       function()
+		  jet = require'jet'
+	       end)
+	 end)
+
+      it(
+	 'jet.daemon is exposed',
+	 function()
+	    assert.is.equal(jet.daemon,require'jet.daemon')
+	    assert.is.equal(type(jet.daemon.new),'function')
+	 end)
+
+      it(
+	 'jet.peer is exposed',
+	 function()
+	    assert.is.equal(jet.peer,require'jet.peer')
+	    assert.is.equal(type(jet.peer.new),'function')
+	    assert.is.same(jet.peer.new,require'jet.peer'.new)
+	 end)
+
+      it(
+	 'jet.new equals jet.peer.new',
+	 function()
+	    assert.is.same(jet.new,require'jet.peer'.new)
+	 end)
+
+   end)
---
+++ src/jet.lua
@@ -0,0 +1,12 @@
+local peer = require'jet.peer'
+local daemon = require'jet.daemon'
+
+module('jet')
+
+local jet = {
+   peer = peer,
+   daemon = daemon,
+   new = peer.new
+}
+
+return jet
