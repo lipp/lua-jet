e76021c2b642739a9b48384fe187f9abf26fd683
Gerhard Lipp <gelipp@gmail.com>
2013-07-05 17:40:24 +0200

    read cmdline

--- examples/fetch.lua
+++ examples/fetch.lua
@@ -1,11 +1,16 @@
 #!/usr/bin/env lua
 -- fetches all available jet nodes,states and methods and prints
 -- the basic notification info
-local jet = require'jet.peer'.new()
+
+local exp = arg[1] or '.*'
+local ip = arg[2]
+local port = arg[3]
+
+local jet = require'jet.peer'.new{ip=ip,port=port}
 local cjson = require'cjson'
 local info = function(path,event,data)
    print(path,event,cjson.encode(data))
 end
-jet:fetch('.*',info)
+jet:fetch(exp,info)
 jet:loop()
 
