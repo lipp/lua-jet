08b85e5632e159429bdb37a911d40b77a109736b
Gerhard Lipp <gelipp@gmail.com>
2013-07-05 17:35:25 +0200

    add command line params

--- bin/jetd.lua
+++ bin/jetd.lua
@@ -2,23 +2,39 @@
 
 local ev = require'ev'
 
+local start_as_daemon = false
+local ws_port = 11123
+local port = 11122
+local ffi
+
+for _,opt in ipairs(arg) do
+  if opt == '-d' or opt == '--daemon' then
+    ffi = require'ffi'
+    if not ffi then
+      log('daemonizing failed: ffi (luajit) is required.')
+      os.exit(1)
+    end
+    ffi.cdef'int daemon(int nochdir, int noclose)'
+    start_as_daemon = true
+  elseif opt:match('%-w(%d+)') then
+    ws_port = opt:match('%-w(%d+)')
+  elseif opt:match('%-p(%d+)') then
+    port = opt:match('%-p(%d+)')
+  elseif opt == '-h' or opt == '--help' then
+    print('usage: jetd.lua [-d|--daemon] [-w<wsport>] [-p<rawport>] [-h|--help]')
+    os.exit(0)
+  end
+end
+
 local daemon = require'jet.daemon'.new
 {
-   ws_port = 8004,
-   port = 11122
+  ws_port = ws_port,
+  port = port
 }
 daemon:start()
 
-for _,opt in ipairs(arg) do
-   if opt == '-d' or opt == '--daemon' then      
-      local ffi = require'ffi'
-      if not ffi then
-         log('daemonizing failed: ffi (luajit) is required.')
-         os.exit(1)
-      end
-      ffi.cdef'int daemon(int nochdir, int noclose)'
-      assert(ffi.C.daemon(1,1)==0)      
-   end
+if start_as_daemon then
+  ffi.C.daemon(1,1)
 end
 
 ev.Loop.default:loop()
