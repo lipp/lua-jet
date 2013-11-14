4ec703263e4c7c90629980c6b8af2285477f1fe7
Gerhard Lipp <gelipp@googlemail.com>
2013-11-14 10:30:22 +0100

    use * instead of .* wildcard
    * is the only wildcard supported yet.

--- examples/fetch.lua
+++ examples/fetch.lua
@@ -2,14 +2,14 @@
 -- fetches all available jet nodes,states and methods and prints
 -- the basic notification info
 
-local exp = arg[1] or '.*'
+local exp = arg[1] or '*'
 local ip = arg[2]
 local port = arg[3]
 
 local cjson = require'cjson'
 local peer = require'jet.peer'.new{ip=ip,port=port}
 
-peer:fetch(exp,function(path,event,data)
+peer:fetch({},function(path,event,data)
     print(path,event,cjson.encode(data))
   end)
 
