7c6fa8e86ef5120b44b0947ca4d36aa93fd4f7f3
Gerhard Lipp <gelipp@gmail.com>
2013-11-25 12:07:48 +0100

    add more tests on partials

--- spec/path_matcher_spec.lua
+++ spec/path_matcher_spec.lua
@@ -28,7 +28,7 @@ describe(
                 match = {
                   '^somepath$'
                 }
-            })            
+            })
           end)
         
         it('matches',function()
@@ -43,6 +43,123 @@ describe(
             assert.is_falsy(match('^somepath$'))
           end)
         
+      end)
+    
+    describe('Multiple exact path matcher',function()
+        local match
+        
+        setup(function()
+            match = pm.new({
+                match = {
+                  '^somepath$',
+                  '^foobar$',
+                }
+            })
+          end)
+        
+        it('matches',function()
+            assert.is_true(match('somepath'))
+            assert.is_true(match('foobar'))
+          end)
+        
+        it('mismatches',function()
+            assert.is_falsy(match('somePath'))
+            assert.is_falsy(match('somepathsomepath'))
+            assert.is_falsy(match('some*path'))
+            assert.is_falsy(match('^somepath'))
+            assert.is_falsy(match('^somepath$'))
+          end)
+        
+      end)
+    
+    describe('A partial path matcher',function()
+        local match
+        
+        setup(function()
+            match = pm.new({
+                match = {
+                  'somewhere'
+                }
+            })
+            
+          end)
+        
+        it('matches',function()
+            assert.is_true(match('somewhere'))
+            assert.is_true(match('somewhereA'))
+            assert.is_true(match('abcsomewhere123'))
+            assert.is_true(match('abcsomewhere'))
+          end)
+        
+        it('mismatches',function()
+            assert.is_falsy(match('somePath'))
+            assert.is_falsy(match('someOOpath'))
+            assert.is_falsy(match('AAsomepath'))
+            assert.is_falsy(match('asomepathb'))
+          end)
+      end)
+    
+    describe('A partial path matcher with exact unmatch',function()
+        local match
+        
+        setup(function()
+            match = pm.new({
+                match = {
+                  'somewhere'
+                },
+                unmatch = {
+                  '^abcsomewhere1234$'
+                }
+            })
+            
+          end)
+        
+        it('matches',function()
+            assert.is_true(match('somewhere'))
+            assert.is_true(match('somewhereA'))
+            assert.is_true(match('abcsomewhere123'))
+            assert.is_true(match('abcsomewhere'))
+          end)
+        
+        it('mismatches',function()
+            assert.is_falsy(match('abcsomewhere1234'))
+            assert.is_falsy(match('somePath'))
+            assert.is_falsy(match('someOOpath'))
+            assert.is_falsy(match('AAsomepath'))
+            assert.is_falsy(match('asomepathb'))
+          end)
+      end)
+    
+    describe('A partial path matcher with partial unmatch',function()
+        local match
+        
+        setup(function()
+            match = pm.new({
+                match = {
+                  'somewhere'
+                },
+                unmatch = {
+                  '1234'
+                }
+            })
+            
+          end)
+        
+        it('matches',function()
+            assert.is_true(match('somewhere'))
+            assert.is_true(match('somewhereA'))
+            assert.is_true(match('abcsomewhere123'))
+            assert.is_true(match('abcsomewhere'))
+          end)
+        
+        it('mismatches',function()
+            assert.is_falsy(match('abcsomewhere1234'))
+            assert.is_falsy(match('1234somewhere'))
+            assert.is_falsy(match('somePath'))
+            assert.is_falsy(match('someOOpath'))
+            assert.is_falsy(match('AAsomepath'))
+            assert.is_falsy(match('asomepathb'))
+          end)
       end)
     
     describe('A left-bound partial path matcher',function()
@@ -67,7 +184,7 @@ describe(
             assert.is_falsy(match('someOOpath'))
             assert.is_falsy(match('AAsomepath'))
             assert.is_falsy(match('asomepathb'))
-          end)        
+          end)
       end)
     
     describe('A right-bound partial path matcher',function()
@@ -78,7 +195,7 @@ describe(
                 match = {
                   'somepath$'
                 }
-            })            
+            })
           end)
         
         it('matches',function()
@@ -91,7 +208,7 @@ describe(
             assert.is_falsy(match('some*path'))
             assert.is_falsy(match('somepathT'))
             assert.is_falsy(match('asomepatho'))
-          end)        
+          end)
       end)
     
     
